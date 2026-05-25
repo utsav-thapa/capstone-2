5824305b5523444f7dac065121008a72639f204d
killioin007-source <killioin.007@gmail.com>
2026-05-25 14:04:21 -0400

    added code for checking if customer wants cheese or not and changed it accordingly

--- src/main/java/Sandwich.java
+++ src/main/java/Sandwich.java
@@ -101,7 +101,37 @@ public class Sandwich {
 //                    throw new RuntimeException("Illegal size");
                 }
 
+                if (!this.cheese.equals(Cheese.NO_CHEESE)){
+                    switch (this.size){
+                        case SMALL:
+                            price += .75;
+                            break;
+                        case MEDIUM:
+                            price += 1.50;
+                            break;
+                        case LARGE:
+                            price += 2.25;
+                            break;
+                        default:
+                            throw new RuntimeException("Do you want cheese or not?");
+                    }
+                }
+                if (this.extraCheese){
+                    switch (this.size){
+                        case SMALL:
+                        price += .30;
+                        break;
+                        case MEDIUM:
+                            price += .60;
+                            break;
+                        case LARGE:
+                            price += .90;
+                            break;
+                        default:
+                            throw new RuntimeException("So you don't want cheese eh?");
+                    }
 
+                }
 
             }
         }
